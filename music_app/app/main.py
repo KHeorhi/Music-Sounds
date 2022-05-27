c01488e82b25e8c96998ff75c6951c14d7140aa6
Yukei <yurykirpichenko@yandex.ru>
2022-05-27 19:37:11 +0300

    image_folder commit

--- music_app/app/main.py
+++ music_app/app/main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from app import App
 
-music_player = App(name_title='Music Sound', path_iconbitmap = 'D:\программирование\Tkinter\music_app\image\mp3_ico.ico', size_geometry='300x300')
+music_player = App(name_title='Music Sound', path_iconbitmap = 'image/mp3_ico.ico', size_geometry='300x300')
 
 music_player.mainloop()
